5c8b8fca78f9bee8d9f3a3f20fb04d09edad379e
David Lopez Jr <42970536+DLopezJr@users.noreply.github.com>
2023-02-12 18:15:41 +0100

    Create fp.sh

---
+++ fp.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+#AUTHOR: David Lopez Jr. /* git(hub|lab).com/dlopezjr */
+#PURPOSE: Frontend wrapper for $(flatpak run)
+
+#VARIABLE(S)
+app=$(flatpak list --app | cut -f2 | awk -F. -v app="$1" '(tolower($NF) ~ app)')
+
+#FUNCTION(S)
+##check if flatpak is installed and accesible to $PATH
+test -x "$(command -v flatpak)" || printf "Flatpak package was not found.\n"
+
+##check if app name was entered by user
+test -z $1 && flatpak list --app && exit 
+
+##main
+flatpak run "$app" "$2"
