4c6dc30a0a27f1e1303b3a7d3a781f8d388f76cd
David Lopez Jr <42970536+DLopezJr@users.noreply.github.com>
2023-02-17 09:17:36 -0600

    Updating test -z comment

--- fp.sh
+++ fp.sh
@@ -9,7 +9,7 @@ app=$(flatpak list --app | cut -f2 | awk -F. -v app="$1" '(tolower($NF) ~ tolowe
 ##check if flatpak is installed and accesible to $PATH
 command -v flatpak || printf "Flatpak package was not found.\n";
 
-##check if app name was entered by user
+##check if app name was NOT entered by user
 test -z $1 && printf "Enter an app to fp.\n\$ fp <app>\n\nINSTALLED APPS\n$app\n" && exit; 
 
 ##remove app name from "$@" array
