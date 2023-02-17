57e08787ef2aed2fd47639c1bebf8e2896cd2c03
David Lopez Jr <42970536+DLopezJr@users.noreply.github.com>
2023-02-17 09:15:46 -0600

    test -x not needed for flatpak

--- fp.sh
+++ fp.sh
@@ -7,7 +7,7 @@ app=$(flatpak list --app | cut -f2 | awk -F. -v app="$1" '(tolower($NF) ~ tolowe
 
 #FUNCTION(S)
 ##check if flatpak is installed and accesible to $PATH
-test -x "$(command -v flatpak)" || printf "Flatpak package was not found.\n";
+command -v flatpak || printf "Flatpak package was not found.\n";
 
 ##check if app name was entered by user
 test -z $1 && printf "Enter an app to fp.\n\$ fp <app>\n\nINSTALLED APPS\n$app\n" && exit; 
