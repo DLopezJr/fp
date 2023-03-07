0f047e4209fa4fffd32c8fdf350cfeb5199b3d88
Anskrevy <95379591+Anskrevy@users.noreply.github.com>
2023-03-07 11:13:38 -0800

    Rearrange safety checks and return 1 on error

--- fp.sh
+++ fp.sh
@@ -2,15 +2,15 @@
 #AUTHOR: David Lopez Jr. /* git(hub|lab).com/dlopezjr */
 #PURPOSE: Frontend wrapper for $(flatpak run)
 
-#VARIABLE(S)
-app=$(flatpak list --app | cut -f2 | awk -F. -v app="$1" '(tolower($NF) ~ tolower(app))')
-
 #FUNCTION(S)
 ##check if flatpak is installed and accesible to $PATH
-command -v flatpak || printf "Flatpak package was not found.\n";
+command -v flatpak || { printf "Flatpak package was not found.\n"; exit 1; }
 
 ##check if app name was NOT entered by user
-test -z $1 && printf "Enter an app to fp.\n\$ fp <app>\n\nINSTALLED APPS\n$app\n" && exit; 
+test -z $1 && printf "Enter an app to fp.\n\$ fp <app>\n\nINSTALLED APPS\n$app\n" && exit 1;
+
+#VARIABLE(S)
+app=$(flatpak list --app | cut -f2 | awk -F. -v app="$1" '(tolower($NF) ~ tolower(app))')
 
 ##remove app name from "$@" array
 shift 1;
