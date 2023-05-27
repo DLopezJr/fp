0af07dc0d5f0008719d7d467c35fe6ce2d8b1b79
David Lopez Jr <42970536+DLopezJr@users.noreply.github.com>
2023-05-27 12:05:22 +0000

    Update fp.sh
    Replaced "test" with "[" in two lines to increase readability.

--- fp.sh
+++ fp.sh
@@ -8,13 +8,13 @@ command -v flatpak >/dev/null 2>&1 || { printf "Flatpak package was not found.\n
 test "$(flatpak list --all | head -c1 | wc -c)" -eq "0" && { printf "No Flatpak packages are installed.\n"; exit 1; }
 
 ##check if an app name was entered
-test "$#" -eq "0" && { printf "Enter an app to fp.\n\$ fp <app>\n\nINSTALLED APPS:\n$(flatpak list --all)\n"; exit 1; }
+[ "$#" -eq "0" ] && { printf "Enter an app to fp.\n\$ fp <app>\n\nINSTALLED APPS:\n$(flatpak list --all)\n"; exit 1; }
 
 ##store reverse dns name for package
 app=$(flatpak list --app | cut -f2 | awk -F. -v app="$1" '(tolower($0) ~ tolower(app))')
 
 ##check if entered app name is valid
-test -z "$app" && { printf "Entered app name is invalid.\n"; exit 1; }
+[ -z "$app" ] && { printf "Entered app name is invalid.\n"; exit 1; }
 
 ##remove app name from "$@" array
 shift 1;
