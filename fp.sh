0d9fdc54cd307eae43631909733ad1ff116c8da2
David Lopez Jr <42970536+DLopezJr@users.noreply.github.com>
2023-06-03 18:27:19 +0000

    Update fp.sh

--- fp.sh
+++ fp.sh
@@ -8,7 +8,7 @@ command -v flatpak >/dev/null 2>&1 || { printf "Flatpak package was not found.\n
 test "$(flatpak list --all | head -c1 | wc -c)" -eq "0" && { printf "No Flatpak packages are installed.\n"; exit 1; }
 
 ##check if an app name was entered
-[ "$#" -eq "0" ] && { printf "Enter an app to fp.\n\$ fp <app>\n\nINSTALLED APPS:\n$(flatpak list --all)\n"; exit 1; }
+[ "$#" -eq "0" ] && { printf "%s\n\n%s\n%s\n%s" "Usage: fp [APP] [OPTION]" "INSTALLED APPS:" "$(flatpak list --all)"; exit 1; }
 
 ##store reverse dns name for package
 app=$(flatpak list --app | cut -f2 | awk -F. -v app="$1" '(tolower($0) ~ tolower(app))')
